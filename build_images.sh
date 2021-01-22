a36204906a9121f71ed7243947da7dee9d531616
Rob Clarke <robert.clarke@bristol.ac.uk>
2021-01-22 23:36:03 +0000

    Add ardupilot build images

--- build_images.sh
+++ build_images.sh
@@ -6,7 +6,8 @@ docker build -t starling-sim-base-core simulator/base/core
 # Build base image for PX4 SITL
 docker build -t starling-sim-base-px4 simulator/base/px4
 
-#docker build -t starling-sim-base-ardupilot simulator/base/ardupilot
+# Build base image for ArduPilot SITL
+docker build -t starling-sim-base-ardupilot simulator/base/ardupilot
 
 # Build overlay image for clover vehicle
 #docker build -t starling-sim-clover simulator/vehicles/clover
