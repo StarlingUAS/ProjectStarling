dd4763dadbd5d15d129bcb3a8c60a19ef45bf2f2
mickey li <mickeyhli@outlook.com>
2021-02-17 17:12:40 +0000

    Created automatic setup script for k3s

---
+++ run_k3s.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+# Download and start kubernetes master node 
+echo "Downloading and Running K3s in systemd"
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--docker" sh -
+
+echo "Deploying Gazebo-iris to 10.43.226.5:8080"
+sudo k3s kubectl apply -f deployment/k8.starling-gazebo.amd64.yaml
+
+echo "Deploying px4-sitl with mavros"
+sudo k3s kubectl apply -f deployment/k8.px4-sitl.amd64.yaml
+
+echo "Deploying Dashboard"
+echo "==================="
+GITHUB_URL=https://github.com/kubernetes/dashboard/releases
+VERSION_KUBE_DASHBOARD=$(curl -w '%{url_effective}' -I -L -s -S ${GITHUB_URL}/latest -o /dev/null | sed -e 's|.*/||')
+sudo k3s kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml
+sudo k3s kubectl apply -f deployment/resources/dashboard.admin-user.yml -f deployment/resources/dashboard.admin-user-role.yml
+sudo k3s kubectl proxy &
+echo "==================="
+
+echo "Here is the dashboard token, use it to log in to the dashboard."
+sudo k3s kubectl -n kubernetes-dashboard describe secret admin-user-token | grep ^token
+
+echo "Opening Dashboard"
+xdg-open http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
+
+echo "Opening gazebo"
+xdg-open http://10.43.226.5:8080
+
