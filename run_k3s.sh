fb34b47263f8ac7f584bddad5ca8ffe9d465e51a
mickey li <mickeyhli@outlook.com>
2021-02-18 12:15:06 +0000

    Issue with k3s-agent on drone reboots. start script now performs an uninstall all, copies relevant files to the correct places and starts the systemd successfully.

--- run_k3s.sh
+++ run_k3s.sh
@@ -4,6 +4,7 @@ set -e
 # Download and start kubernetes master node 
 echo "Downloading and Running K3s in systemd"
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--docker" sh -
+systemctl start k3s
 
 echo "Deploying Gazebo-iris to 10.43.226.5:8080"
 sudo k3s kubectl apply -f deployment/k8.starling-gazebo.amd64.yaml
@@ -17,7 +18,7 @@ GITHUB_URL=https://github.com/kubernetes/dashboard/releases
 VERSION_KUBE_DASHBOARD=$(curl -w '%{url_effective}' -I -L -s -S ${GITHUB_URL}/latest -o /dev/null | sed -e 's|.*/||')
 sudo k3s kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml
 sudo k3s kubectl apply -f deployment/resources/dashboard.admin-user.yml -f deployment/resources/dashboard.admin-user-role.yml
-sudo k3s kubectl proxy &
+sudo k3s kubectl proxy 2>/dev/null & 
 echo "==================="
 
 echo "Here is the dashboard token, use it to log in to the dashboard."
