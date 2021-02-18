ea653103f9150923f85588cd43a906310a321f7e
Mickey Li <mickeyhli@outlook.com>
2021-02-18 17:20:16 +0000

    Kubernetes Deploy Mavros, Gazebo and Px4-SITL (#13)
    * Included IP parsing as SYSID for mavros, Included ardupilot_sitl
    
    * Ensured launch.sh is executable
    
    * Added ardupilot_ar to simulator makefile
    
    * Updated Mavros README.md
    
    * Removed launch.sh env variables now set as edfault for launchfile.
    
    * Created initial deployment scripts from other branch
    
    * Deployment testing and README.md
    
    * Added shareProcessNamespace to avoid ROS dds guid clashing
    
    * Moved testing code to testing folder
    
    * Created ros_debug_testing node
    
    * Found a fix for kubernetes networking issues
    
    * Adjusted to use airgapped k3s install on agent. Also script adds 1 agent at a time
    
    * Updated Makefiles and changed agent start_k3s_name name
    
    * Updated README.md files, moved some content around
    
    * Removed setup_script.sh from ros_debug
    
    * Added prereqs properly to makefiles, also ensured pushes and k8 pulls go to uobflightlabstarling
    
    * Added gcs_url to mavros launch file defaulted to udp-pb 14553 to allow gcs
    
    * Updated k8 naming schema and working mavros arm64 deployment
    
    * Gazebo deployment
    
    * Removed fcu_url from dockerfile cmd and set as default env as overwriting environment variable
    
    * Gazebo, px4 and mavros now work inside k3s and deployed
    
    * Removed old controller template and resolved merge conflict
    
    * Remaining merge conflict
    
    * Created automatic setup script for k3s
    
    * Updated README files
    
    * Added push to ardupilot-ar
    
    * Updated readme
    
    * Issue with k3s-agent on drone reboots. start script now performs an uninstall all, copies relevant files to the correct places and starts the systemd successfully.
    
    * Refer to previous commit
    
    * Fixed typo
    
    * Removed mavros ros2 launch args
    
    * Fixed typo in system makefile
    
    * Moved env varibles in Dockerfile
    
    * controller dockerfile now calls hub created controller k8 launch

--- deployment/start_k3s_agent.sh
+++ deployment/start_k3s_agent.sh
@@ -7,6 +7,8 @@ then
     echo "Usage: ./run_k3s_agent.sh <Remote (Root enabled) Username> <Remote IP Address> <K3S Node Name>"
     echo "e.g ./start_k3s_agent ubuntu 192.168.0.110 clover1"
     echo "e.g. K3S_SERVER=https://192.168.0.63:6443 k3s_agent ubuntu 192.168.0.96 raspi1"
+    echo ""
+    echo "Note: It is assumed that the k3s-airgap-images-arm64.tar and the k3s-arm64 files have been downloaded from the k3s github releases and are in the home directory named as such."
     exit -1
 fi
 
@@ -20,6 +22,13 @@ echo "K3S Token is $K3S_TOKEN"
 echo "K3S Server is $K3S_SERVER"
 echo "Connecting to $2 with user $1, starting k3s agent node named $3"
 
-ssh $1@$2 "INSTALL_K3S_SKIP_DOWNLOAD=true K3S_NODE_NAME=$3 K3S_URL=${K3S_SERVER} K3S_TOKEN=${K3S_TOKEN} INSTALL_K3S_EXEC=${INSTALL_K3S_EXEC} ./k3s-install.sh;";
+ssh $1@$2 " \
+ k3s-agent-uninstall.sh;\
+ sudo mkdir -p /var/lib/rancher/k3s/agent/images/; \
+ sudo cp k3s-airgap-images-arm64.tar /var/lib/rancher/k3s/agent/images/; \ 
+ sudo chmod +x k3s-arm64; \
+ sudo cp k3s-arm64 /usr/local/bin/k3s; \
+ INSTALL_K3S_SKIP_DOWNLOAD=true K3S_NODE_NAME=$3 K3S_URL=${K3S_SERVER} K3S_TOKEN=${K3S_TOKEN} INSTALL_K3S_EXEC=${INSTALL_K3S_EXEC} ./k3s-install.sh;";
+
 
 exit -1
