44b17eb66c41c6754f97cf39bb9f0713406463f6
Mickey Li <mickeyhli@outlook.com>
2021-02-26 13:51:26 +0000

    Kubernetes Fixes and Verified Example Controller Runs (#20)
    * Fixed dashboard certificates problem by creating NodePort service
    
    * Added automatic copying of dashboard token into clipboard
    
    * Example confirmed to work, example controller now uses ros logging, added further explanation to deployment file
    
    * Updated README.md
    
    * Gazebo now exposes to localhost 8080 and ui exposes to localhost 3000
    
    * Renamed gazebo deployment file
    
    * Updated README.md
    
    * Updated deployment README
    
    * Address issues raised

--- controllers/README.md
+++ controllers/README.md
@@ -5,18 +5,32 @@ begins sending offboard setpoints, sets the mode to `OFFBOARD`, arms the
 vehicle, and finally begins moving the setpoint around the sky.
 
 ## Running
+### Docker
 
 To run the controller, it needs to be connected to the network that the other ROS nodes exist in.
+
 For example:
 ```bash
-docker run -it --network starling_default example_controller_python
+docker run -it --network projectstarling_default example_controller_python
 ```
+where `projectstarling_default` is the name of the default network created by the example `docker-compose.yaml` in the root directory.
+
+Replace `example_controller_python` with `uobflightlabstarling/example_controller_python` if you have not locally built the controller. 
+
+### Kubernetes
+
+To run the controller in kubernetes, k3s must be up and running, then simply apply the k8 deployment script
+```bash
+kubectl apply -f example_controller_python/k8.example_controller_python.amd64.yaml
+```
+
+This will firstly run the local `uobflightlabstarling/example_controller_python` if one exists, otherwise it will pull from docker hub. 
 
 ## TODO
 
 - [ ] Get it working for the clover model
-- [ ] Add support for a land command or similar
-- [ ] Add support for an emergency stop
+- [x] Add support for a land command or similar
+- [x] Add support for an emergency stop
 
 ## Notes
 Due to the way rclpy works, this container can not be killed gracefully unless
--- run_k3s.sh
+++ run_k3s.sh
@@ -2,39 +2,52 @@
 set -e
 
 # Download and start kubernetes master node 
-echo "Downloading and Running K3s in systemd"
+echo "Downloading and Running K3s in systemd (Will not do anything if k3s already installed and running"
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--docker" sh -
 
-echo "Deploying Gazebo-iris to 10.43.226.5:8080 and wait for start"
-sudo k3s kubectl apply -f deployment/k8.starling-gazebo.amd64.yaml
-
-sleep 5s
-
-echo "Deploying px4-sitl with mavros"
-sudo k3s kubectl apply -f deployment/k8.px4-sitl.amd64.yaml
-
-echo "Deploying web ui"
-sudo k3s kubectl apply -f system/ui/kubernetes.yaml
-
-echo "Deploying Dashboard"
+echo "Deploying Kubernetes Dashboard"
+echo "See: https://blog.tekspace.io/deploying-kubernetes-dashboard-in-k3s-cluster/ for more details"
 echo "==================="
 GITHUB_URL=https://github.com/kubernetes/dashboard/releases
 VERSION_KUBE_DASHBOARD=$(curl -w '%{url_effective}' -I -L -s -S ${GITHUB_URL}/latest -o /dev/null | sed -e 's|.*/||')
 sudo k3s kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml
-sudo k3s kubectl apply -f deployment/resources/dashboard.admin-user.yml -f deployment/resources/dashboard.admin-user-role.yml
-sudo k3s kubectl proxy 2>/dev/null & 
+sudo k3s kubectl apply -f deployment/resources/dashboard.admin.yaml #dashboard.admin-user.yml -f deployment/resources/dashboard.admin-user-role.yml
 echo "==================="
 
 echo "Here is the dashboard token, use it to log in to the dashboard."
-sudo k3s kubectl -n kubernetes-dashboard describe secret admin-user-token | grep ^token
+DASHBOARD_TOKEN=`sudo k3s kubectl -n kubernetes-dashboard describe secret admin-user-token | grep ^token | cut -c 13-`
+echo $DASHBOARD_TOKEN
+echo $DASHBOARD_TOKEN | xclip -selection clipboard -i
+echo "The token has been copied onto your clipboard"
+echo "Note: your browser may not like the self signed ssl certificate, ignore and containue for now"
+echo "To get the token yourself run: sudo k3s kubectl -n kubernetes-dashboard describe secret admin-user-token"
+echo "==================="
 
-echo "Opening Starling UI"
-xdg-open http://localhost:30000
+echo "Deploying Starling Modules (this may take a while)"
+echo "Deploying Gazebo-iris to localhost:8080 and wait for start"
+sudo k3s kubectl apply -f deployment/k8.gazebo-iris.amd64.yaml
 
-echo "Opening gazebo"
-xdg-open http://10.43.226.5:8080
+echo "Deploying web ui"
+sudo k3s kubectl apply -f system/ui/kubernetes.yaml
 
-echo "Opening Dashboard"
-xdg-open http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
+echo "Waiting 20s for Gazebo to start to ensure proper connection"
+sleep 20s
 
+echo "Deploying px4-sitl with mavros"
+sudo k3s kubectl apply -f deployment/k8.px4-sitl.amd64.yaml
 
+echo "Wait for containers to initialise"
+sleep 10s
+
+echo "Opening Starling UI to http://localhost:3000/html/main.html"
+xdg-open http://localhost:3000/html/main.html
+
+echo "Opening gazebo to http://localhost:8080"
+xdg-open http://localhost:8080
+
+echo "Opening Dashboard to https://localhost:31771"
+xdg-open https://localhost:31771
+
+echo "All actions completed"
+echo "k3s will run in the background systemd permanently"
+echo "run 'k3s-uninstall.sh' to remove all of k3s"
