9607727fbba0eee5c68b430b504b5f455ac548f5
mickey li <mickeyhli@outlook.com>
2021-03-03 21:34:36 +0000

    Added ip parsing as default

--- simulator/base/px4/entrypoint.sh
+++ simulator/base/px4/entrypoint.sh
@@ -16,4 +16,14 @@ else
     echo "PX4_OFFBOARD_HOST not set, assuming loopback"
 fi
 
+# If PX4_INSTANCE is zero, then set instance id to IP address last digit
+if [ "$PX4_INSTANCE" -eq "0" ]; then
+    IPADDR=`ifconfig eth0  | grep 'inet' | awk '{print $2}'`
+    read A B C D <<< "${IPADDR//./ }"
+    export PX4_INSTANCE=$D;
+    echo "PX4_INSTANCE set to $D from IP ADDRESS: $IPADDR (was zero)"
+else
+    echo "PX4_INSTANCE setting as specified: $PX4_INSTANCE"
+fi
+
 /ros_entrypoint.sh "$@"
--- system/mavros/mavros_setup.sh
+++ system/mavros/mavros_setup.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-# MAVROS_TGT_SYSTEM may come in as an IP Address
-for i in ${MAVROS_TGT_SYSTEM} ; do
-    if [[ $i == *.* ]]; then
-        read A B C D <<<"${MAVROS_TGT_SYSTEM//./ }"
-        echo "MAVROS TGT IS IN IP ADDRESS FORM (${MAVROS_TGT_SYSTEM}), WILL EXTRACT FINAL DIGIT FOR USE"
-        export MAVROS_TGT_SYSTEM=$D;
-        break;
+# If MAVROS_TGT_SYSTEM is zero, then set instance id to IP address last digit
+if [ "$MAVROS_TGT_SYSTEM" -eq "0" ]; then
+    if [ -f "/etc/drone.config"]; then 
+        echo "MAVROS_TGT_SYSTEM will be read from file not yet implemented, set to 1 for now"
+        MAVROS_TGT_SYSTEM=1
+    else
+        IPADDR=`ifconfig eth0  | grep 'inet' | awk '{print $2}'`
+        read A B C D <<< "${IPADDR//./ }"
+        MAVROS_TGT_SYSTEM=$D;
+        echo "MAVROS_TGT_SYSTEM set to $D from IP ADDRESS: $IPADDR (was zero and /etc/drone.config not found)"
     fi
-done
+else
+    echo "MAVROS_TGT_SYSTEM setting as specified: $MAVROS_TGT_SYSTEM"
+fi
+
+export MAVROS_TGT_SYSTEM=$MAVROS_TGT_SYSTEM
