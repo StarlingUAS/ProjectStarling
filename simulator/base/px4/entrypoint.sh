e4ff941c39fe3c28ebb35c7e41b625bbcdb70e00
Rob Clarke <robert.clarke@bristol.ac.uk>
2021-03-19 20:39:26 +0000

    Update SITL entrypoint with extra debug info

--- simulator/base/px4/entrypoint.sh
+++ simulator/base/px4/entrypoint.sh
@@ -7,6 +7,7 @@ if [ -n "$PX4_SIM_HOST" ]; then
 else
     echo "PX4_SIM_HOST not set, assuming loopback"
 fi
+echo "Setting PX4_SIM_IP to $PX4_SIM_IP"
 
 # If PX4_OFFBOARD_HOST is set, lookup IP for offboard
 if [ -n "$PX4_OFFBOARD_HOST" ]; then
@@ -15,6 +16,7 @@ if [ -n "$PX4_OFFBOARD_HOST" ]; then
 else
     echo "PX4_OFFBOARD_HOST not set, assuming loopback"
 fi
+echo "Setting PX4_OFFBOARD_IP to $PX4_OFFBOARD_IP"
 
 # If PX4_INSTANCE is "ordinal", then set instance id to take PX4_INSTANCE_BASE + ORDINAL from StatefulSet hostname
 # Therefore SYSID is set to PX4_INSTANCE_BASE + ORDINAL + 1
@@ -22,7 +24,7 @@ fi
 if [ "$PX4_INSTANCE" == "ordinal" ]; then
     ORDINAL="${HOSTNAME##*-}"
     export PX4_INSTANCE=$((PX4_INSTANCE_BASE + ORDINAL));
-    echo "PX4_INSTANCE was 'ordinal' therefore set to $PX4_INSTANCE (from hostname: $HOSTNAME)"
+    echo "PX4_INSTANCE was 'ordinal' therefore set to $PX4_INSTANCE (from base: $PX4_INSTANCE_BASE and hostname: $HOSTNAME)"
 elif (($PX4_INSTANCE >= 0 && $PX4_INSTANCE <= 254 )); then
     echo "PX4_INSTANCE setting as specified: $PX4_INSTANCE"
 else
