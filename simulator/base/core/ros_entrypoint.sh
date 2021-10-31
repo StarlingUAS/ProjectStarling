6dbb4a6c9ed7a6481d5ab18c0df6fc027a6b92c4
mickey li <mickeyhli@outlook.com>
2021-10-31 11:17:37 +0000

    Added support for numbering 'xx_' in ros.env.d

--- simulator/base/core/ros_entrypoint.sh
+++ simulator/base/core/ros_entrypoint.sh
@@ -11,8 +11,10 @@ source /setup_display.sh
 source /ros.env
 
 # Attempt to source additional env files (possibly volume mounted)
-for folder in $(find /ros.env.d -mindepth 1 -maxdepth 1 -type d -name "[!.]*"); do
+ROSENVD_LIST=$(find /ros.env.d -mindepth 1 -maxdepth 1 -type d -name "[!.]*")
+for folder in $(echo $ROSENVD_LIST | xargs -n1 | sort | xargs ); do
     if [ -f ${folder}/setup.bash ]; then
+        echo "---- ros_entrypoint.sh: sourcing ${folder}/setup.bash -----"
         source ${folder}/setup.bash
     fi
 done
