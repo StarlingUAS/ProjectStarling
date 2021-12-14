1f1406db319a45c74fed538e3ef56d00e1078d8b
Robert Clarke <robert.clarke@bristol.ac.uk>
2021-12-14 16:18:55 +0000

    Add rm to docker runs

--- tests/system/mavros/interior_tests/test_get_instance.sh
+++ tests/system/mavros/interior_tests/test_get_instance.sh
@@ -5,6 +5,7 @@ set -e
 MAVROS_IMAGE=uobflightlabstarling/starling-mavros
 
 # Test get_instance
-docker run $MAVROS_IMAGE bash -c 'source /ros_ws/mavros_setup.sh; INSTANCE=$(get_instance); [ $? -eq 2 ]; [ $INSTANCE -eq 0 ]'
-docker run $MAVROS_IMAGE bash -c 'source /ros_ws/mavros_setup.sh; INSTANCE=$(HOSTNAME=test-test get_instance); [ $? -eq 3 ]; [ $INSTANCE -eq 0 ]'
-docker run $MAVROS_IMAGE bash -c 'source /ros_ws/mavros_setup.sh; INSTANCE=$(HOSTNAME=test-5 get_instance); [ $? -eq 0 ];# [ $INSTANCE -eq 5 ]'
+docker run --rm $MAVROS_IMAGE bash -c 'source /ros_ws/mavros_setup.sh; INSTANCE=$(get_instance); [ $? -eq 2 ]; [ $INSTANCE -eq 0 ]'
+docker run --rm $MAVROS_IMAGE bash -c 'source /ros_ws/mavros_setup.sh; INSTANCE=$(HOSTNAME=test-test get_instance); [ $? -eq 3 ]; [ $INSTANCE -eq 0 ]'
+docker run --rm $MAVROS_IMAGE bash -c 'source /ros_ws/mavros_setup.sh; INSTANCE=$(HOSTNAME=test-5 get_instance); [ $? -eq 0 ]; [ $INSTANCE -eq 5 ]'
+docker run --rm $MAVROS_IMAGE bash -c 'source /ros_ws/mavros_setup.sh; INSTANCE=$(PX4_INSTANCE_BASE=10 HOSTNAME=test-5 get_instance); [ $? -eq 0 ]; [ $INSTANCE -eq 15 ]'
