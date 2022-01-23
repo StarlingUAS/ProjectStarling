7617f12b1ee7c2a84f7cc12599f1aded2a1af2f3
Mickey Li <mickeyhli@outlook.com>
2022-01-23 23:47:24 +0000

    Created temporary 1.0.7 dockerfile for foxglove compatibility

--- system/rosbridge-suite/run.sh
+++ system/rosbridge-suite/run.sh
@@ -6,6 +6,7 @@ if [ ! -d "${MSGS_WS}" ]; then
     echo "No external messages mounted at ${MSGS_WS}"
 elif [ -d "${MSGS_WS}/install" ]; then
     echo "Messages already built at ${MSGS_WS}/install, not building"
+    source "${MSGS_WS}/install/setup.bash"
 else
     echo "External messages detected at ${MSGS_WS}, building..."
     colcon build --base-paths "${MSGS_WS}" --build-base "${MSGS_WS}/build" --install-base "${MSGS_WS}/install"
@@ -13,4 +14,11 @@ else
     source "${MSGS_WS}/install/setup.bash"
 fi
 
+# Check if extra /install in ros_ws, if so source it.
+if [ -d "/ros_ws/install" ]; then
+    echo "Install folder exists in ros_ws, sourcing setup"
+    source "/ros_ws/install/setup.bash"
+fi
+
+ros2 pkg list
 ros2 launch /ros_ws/rosbridge_websocket_launch.xml port:=${ROSBRIDGE_PORT}
