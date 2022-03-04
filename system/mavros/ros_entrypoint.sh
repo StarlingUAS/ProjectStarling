e48b4ad9986770288193d5c5d7c0494e3dd28754
Rob Clarke <robert.clarke@bristol.ac.uk>
2022-03-04 17:12:59 +0000

    Add mavros from dev

---
+++ system/mavros/ros_entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# unsetting ROS_DISTRO to silence ROS_DISTRO override warning
+unset ROS_DISTRO
+# setup ros1 environment
+source "/opt/ros/$ROS1_DISTRO/setup.bash"
+
+# unsetting ROS_DISTRO to silence ROS_DISTRO override warning
+unset ROS_DISTRO
+# setup ros2 environment
+source "/opt/ros/$ROS2_DISTRO/setup.bash"
+
+# Source the from-source ros1_bridge workspace
+source "/bridge_ws/install/setup.bash"
+
+# Source the mavros_setup for any user defined edits to the environment
+source "/ros_ws/mavros_setup.sh"
+
+exec "$@"
---
+++ system/mavros/run_ros1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /opt/ros/${ROS1_DISTRO}/setup.bash
+
+exec "$@"
